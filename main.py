e5461ceccd48ffce7a878ac590abb1f329dd3212
Verbalist <enshteynn@ukr.net>
2016-03-30 13:13:34 +0000

    a

--- main.py
+++ main.py
@@ -5,77 +5,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def flood_fill(_x, _y, color, image, white=(255, 255, 255)):
-    if image.getpixel((_x, _y)) != white:
-        return None
-    return_set = []
-    fill_set = set()
-    fill_set.add((_x, _y))
-    (a, b, c) = image.getpixel((_x, _y))
-    while fill_set:
-        (x, y) = fill_set.pop()
-        (a, b, c) = image.getpixel((x, y))
-        if not (a, b, c) == white:
-            continue
-        image.putpixel((x, y), color)
-        return_set.append((x, y))
-        if x > 1:
-            fill_set.add((x - 1, y))
-        if x < image.size[0] - 1:
-            fill_set.add((x + 1, y))
-        if y > 1:
-            fill_set.add((x, y - 1))
-        if y < image.size[1] - 1:
-            fill_set.add((x, y + 1))
-    # if not return_set:
-        # print(_x, _y, image.getpixel((_x, _y)))
-    return return_set
 
 
-def seeds(mas, img, white=255):
 
-    # def rec(i, j, vec):
-    #     try:
-    #
-    #
-    #         # print(vec)
-    #         # if mas[i][j] == white:
-    #         #     # mas[i][j] = color_mas[i][j]
-    #         #     mas[i][j] = c
-    #         #     if vec != 1:
-    #         #         if i > 0:
-    #         #             rec(i - 1, j, 2)
-    #         #     if vec != 2:
-    #         #         if i < max_i - 1:
-    #         #             rec(i + 1, j, 1)
-    #         #     if vec != 3:
-    #         #         if j > 0:
-    #         #             rec(i, j - 1, 4)
-    #         #     if vec != 4:
-    #         #         if j < max_j - 1:
-    #         #             rec(i, j + 1, 3)
-    #     except IndexError as e:
-    #         print(e, i, j, max_i, max_j)
-    #     except RuntimeError as e:
-    #         print(e, i, j, max_i, max_j)
-
-    for _i, m in enumerate(mas):
-        for _j, im in enumerate(m):
-            # c += 5
-            if im == white:
-                # rec(_i, _j, 0)
-                floodFill(_j, _i, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), img)
+def get_threshold():
+    pass
 
 
-if __name__ == '__main__':
-    images = subprocess.check_output(['ls images'], shell=True).decode().split('\n')
-    images = {x.split('.')[0]: cv2.imread('images/' + x, 0) for x in images if x.startswith('g')}
-    graphs = {k: [[255 if x[i] == 255 else 0 for i in range(len(x))] for x in v] for k, v in images.items()}
-    try:
-        seeds(graphs['g1'], len(graphs['g1']), len(graphs['g1'][0]))
-    except Exception as e:
-        print(type(e))
-        pass
-    # seeds(graphs['g2'], *images['g2'].shape)
-    plt.imshow(graphs['g1'], cmap='gray')
-    plt.show()
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     images = subprocess.check_output(['ls images'], shell=True).decode().split('\n')
+#     images = {x.split('.')[0]: cv2.imread('images/' + x, 0) for x in images if x.startswith('g')}
+#     graphs = {k: [[255 if x[i] == 255 else 0 for i in range(len(x))] for x in v] for k, v in images.items()}
+#     try:
+#         seeds(graphs['g1'], len(graphs['g1']), len(graphs['g1'][0]))
+#     except Exception as e:
+#         print(type(e))
+#         pass
+#     # seeds(graphs['g2'], *images['g2'].shape)
+#     plt.imshow(graphs['g1'], cmap='gray')
+#     plt.show()
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from PIL import Image
 from PIL.ImageDraw import Draw
 import PIL.ImageFont
 from matplotlib import pyplot as plt
-from main import flood_fill
 import random
+from sklearn.cluster import k_means, MeanShift, estimate_bandwidth, KMeans
 
 MEDIA_ROOT = 'images/'
 im_name = 'c.jpg'
@@ -11,23 +11,131 @@ watermark = 'watermark.png'
 
 
 img = Image.open(MEDIA_ROOT + str(im_name))
-color_img = Image.open(str('test3.jpg'))
-w, h = img.size
 
-pix = list(img.getdata())
 
-a = []
-for x in range(int(len(pix)/w)):
-    a.append([])
-    for y in range(w):
-        a[-1].append(pix[x*w + y])
+def get_mas(_img):
+    pix = list(_img.getdata())
+    a = []
+    for x in range(int(len(pix)/w)):
+        a.append([])
+        for y in range(w):
+            a[-1].append(pix[x*w + y])
+    return a
 
 
 color_map = {}
+white_map = {}
+
+
+def flood_fill(_x, _y, color, image, white=(255, 255, 255)):
+    if image.getpixel((_x, _y)) != white:
+        return None
+    return_set = []
+    fill_set = set()
+    fill_set.add((_x, _y))
+    # (a, b, c) = image.getpixel((_x, _y))
+    while fill_set:
+        (x, y) = fill_set.pop()
+        (a, b, c) = image.getpixel((x, y))
+        if not (a, b, c) == white:
+            continue
+        image.putpixel((x, y), color)
+        return_set.append((x, y))
+        if x > 1:
+            fill_set.add((x - 1, y))
+        if x < image.size[0] - 1:
+            fill_set.add((x + 1, y))
+        if y > 1:
+            fill_set.add((x, y - 1))
+        if y < image.size[1] - 1:
+            fill_set.add((x, y + 1))
+    # if not return_set:
+        # print(_x, _y, image.getpixel((_x, _y)))
+    return return_set
+
+
+def get_info_area(area):
+    return {
+        'max_i': max(area, key=lambda _: _[0]),
+        'min_i': min(area, key=lambda _: _[0]),
+        'max_j': max(area, key=lambda _: _[1]),
+        'min_j': min(area, key=lambda _: _[1])
+    }
+
+
+def set_not_white(_img, _h, _w, white=(255, 255, 255)):
+    for _i in range(_h):
+        for _j in range(_w):
+            if _img.getpixel((_j, _i)) == white:
+                _img.putpixel((_j, _i), (254, 254, 254))
+
+
+def get_c_c(_img, _h, _w, white=(255, 255, 255)):
+    a = set()
+    for _i in range(_h):
+        for _j in range(_w):
+            _pix = _img.getpixel((_j, _i))
+            if _pix not in a:
+                a.add(_pix)
+    return a
+
+
+def threshold(_img, _h, _w, t=20, white=(255, 255, 255)):
+    for _i in range(_h):
+        for _j in range(_w):
+            _pix = _img.getpixel((_j, _i))
+            if _pix != white:
+                white_map[(_j, _i)] = flood_fill(_j, _i, white, _img, white=_pix)
+
+    for _pix, area in white_map.items():
+        if len(area) > t:
+            area.sort(key=lambda _: _[1])
+            c = area[0][1]
+            info = get_info_area(area)
+            min_x = info['max_i']
+            max_x = info['min_i']
+            for x in area:
+                print('x', x)
+                if x[1] == c:
+                    if x[1] in (info['max_j'][1], info['min_j'][1]):
+                        _img.putpixel(x, (0, 0, 0))
+                    else:
+                        if min_x[0] >= x[0]:
+                            min_x = x
+                        if max_x[0] <= x[0]:
+                            max_x = x
+                else:
+                    print(max_x)
+                    print(min_x)
+                    if x[1] not in (info['max_j'], info['min_j']):
+                        _img.putpixel(min_x, (0, 0, 0))
+                        _img.putpixel(max_x, (0, 0, 0))
+                        c = x[1]
+                        min_x = info['max_i']
+                        max_x = info['min_i']
+            # for x in area:
+            #     _img.putpixel(x, color_img_d.getpixel(x))
+
+
+def set_black(_img, _h, _w, white=(255, 255, 255)):
+    for _i in range(_h):
+        for _j in range(_w):
+            if _img.getpixel((_j, _i)) != white:
+                _img.putpixel((_j, _i), (0, 0, 0))
+
+
+def set_k_colors(_img, _h, _w, k):
+    X = [list(x) for x in get_c_c(color_img, h, w)]
+    K = KMeans(n_clusters=k)
+    K.fit(X)
+    color_set = {i: (int(x[0]), int(x[1]), int(x[2])) for i, x in enumerate(K.cluster_centers_)}
+    color_set = {tuple(X[i]): color_set[x] for i, x in enumerate(K.predict(X))}
+    for _i in range(_h):
+        for _j in range(_w):
+            _img.putpixel((_j, _i), color_set[_img.getpixel((_j, _i))])
 
 
 def seeds(mas, _img, white=(255, 255, 255)):
-
     for _i, m in enumerate(mas):
         for _j, im in enumerate(m):
             if im == white:
@@ -37,15 +145,18 @@ def seeds(mas, _img, white=(255, 255, 255)):
                     color_map[(_j, _i)] = ret
 
 
-def set_number(img, _draw, f_size=20):
+def seed_filter(img):
+    pass
+
+
+def set_number(_img, _draw, f_size=20):
     font = PIL.ImageFont.truetype("micross.ttf", size=f_size)
 
     for pixel, r_set in color_map.items():
         color = color_img.getpixel(pixel)
         for re_pix in r_set:
-            img.putpixel(re_pix, (255, 255, 255))
+            _img.putpixel(re_pix, color)
         if len(r_set) > 50:
-            print(color)
             max_i = max(r_set, key=lambda _: _[0])[0]
             min_i = min(r_set, key=lambda _: _[0])[0]
             max_j = max(r_set, key=lambda _: _[1])[1]
@@ -55,10 +166,40 @@ def set_number(img, _draw, f_size=20):
 
         # print((x, y))
 
+# color_img = Image.open(MEDIA_ROOT + str('5.png'))
+color_img = Image.open(MEDIA_ROOT + str('c2.jpg'))
+color_img_d = color_img.copy()
+w, h = color_img.size
 
-seeds(a, img, (255, 255, 255))
-draw = Draw(img)
-set_number(img, draw)
+print('___ img read ___')
 
-plt.imshow(img)
+# bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+# ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+# labels = ms.labels_
+# cluster_centers = ms.cluster_centers_
+
+
+set_k_colors(color_img, h, w, 30)
+
+print('___ set 30 colors ___')
+
+# a = get_c_c(color_img, h, w)
+# for x in a:
+#     print(x)
+# print(len(a))
+# print('___ get count colors ___')
+
+
+# set_not_white(color_img, h, w)
+
+threshold(color_img, h, w, t=20)
+
+# set_black(img, h, w)
+# a = get_mas()
+# seeds(a, img, (255, 255, 255))
+# draw = Draw(img)
+# set_number(img, draw, f_size=10)
+#
+plt.imshow(color_img)
+plt.savefig(MEDIA_ROOT + '30_color_mask.png')
 plt.show()
